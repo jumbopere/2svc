6e594dc3081651430751156c0551379c694185a9
Jumbo-Pere <jumbopere@punch.cool>
2022-05-03 12:25:24 +0100

    resturct file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from './routes';
 
-const {json, urlencoded } = bodyParser;
+const { json, urlencoded } = bodyParser;
 const app = express();
 const PORT = process.env.PORT
 
@@ -11,15 +11,15 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-app.get('/v1', (req, res)=> {
-    res.json('You have 2svc Api enpoints right here')
+app.get('/v1', (req, res) => {
+  res.json('You have 2svc Api enpoints right here')
 })
 app.use('/v1', userRoutes(express))
 
 
 app.listen(PORT, (err) => {
-    if (err) {
-      throw new Error(err.message);
-    }
-    console.log(`server is running on port ${PORT}`);
-  });
+  if (err) {
+    throw new Error(err.message);
+  }
+  console.log(`server is running on port ${PORT}`);
+});
